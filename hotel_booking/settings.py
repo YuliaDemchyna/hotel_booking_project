409044fd28f461b0ba3b09f961da8376dcb398a8
Yulia Demchyna <yuliademchyna@gmail.com>
2024-11-12 10:53:04 +0100

    changed allowed hosts for deployment

--- hotel_booking/settings.py
+++ hotel_booking/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5@npm7%f0e_p01%1%j*=nbs_(lh05v)cec6*&j5g18s+$dx&d(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
